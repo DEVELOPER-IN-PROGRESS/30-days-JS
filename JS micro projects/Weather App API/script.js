9e4b4a423b19a49292e8922336fb557ad3912237
iceweasel <johnsjosephk@protonmail.com>
2021-06-09 23:54:23 +0530

    fixed multiple list element

--- JS micro projects/Weather App API/script.js
+++ JS micro projects/Weather App API/script.js
@@ -5,58 +5,29 @@ const list = document.querySelector(".ajax-section .cities");
 
 const apiKey = "7c1c545a5e3dcb14a9666ed9c564ac28";
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
+function submitAction(e){
+ 
   let inputVal = select.value;
 
   //check if there's already a city
   const listItems = list.querySelectorAll(".ajax-section .city");
   const listItemsArray = Array.from(listItems);
 
-  // if (listItemsArray.length > 0) {
-  //   const filteredArray = listItemsArray.filter(el => {
-  //     let content = "";
-  //     //athens,gr
-  //     if (inputVal.includes(",")) {
-  //       //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-  //       if (inputVal.split(",")[1].length > 2) {
-  //         inputVal = inputVal.split(",")[0];
-  //         content = el
-  //           .querySelector(".city-name span")
-  //           .textContent.toLowerCase();
-  //       } else {
-  //         content = el.querySelector(".city-name").dataset.name.toLowerCase();
-  //       }
-  //     } else {
-  //       //athens
-  //       content = el.querySelector(".city-name span").textContent.toLowerCase();
-  //     }
-  //     return content == inputVal.toLowerCase();
-  //   });
-
-  //   if (filteredArray.length > 0) {
-  //     msg.textContent = `You already know the weather for ${
-  //       filteredArray[0].querySelector(".city-name span").textContent
-  //     } ...otherwise be more specific by providing the country code as well 😉`;
-  //     form.reset();
-  //     select.focus();
-  //     return;
-  //   }
-  // }
-
   //ajax
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
   fetch(url)
-    .then(response => response.json())
+    .then(response =>  response.json()   )
+    
     .then(data => {
       const { main, name, sys, weather } = data;
       const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
         weather[0]["icon"]
       }.svg`;
 
-      const li = document.createElement("li");
+      const li = document.querySelector("li");
       li.classList.add("city");
+      
       const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
@@ -80,4 +51,8 @@ form.addEventListener("submit", e => {
   msg.textContent = "";
   form.reset();
   select.focus();
-});
+
+  e.preventDefault();
+}
+
+form.addEventListener("submit", submitAction);
