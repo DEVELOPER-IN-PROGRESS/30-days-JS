634af7b60108a4fc8cf7b726899825400205617f
iceweasel <johnsjosephk@protonmail.com>
2021-06-10 23:32:12 +0530

    localstorage delegation

--- JS micro projects/Weather App API/script.js
+++ JS micro projects/Weather App API/script.js
@@ -3,6 +3,7 @@ const select = document.querySelector(".top-banner select");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
+//place your API key HERE
 const apiKey = "7c1c545a5e3dcb14a9666ed9c564ac28";
 
 function submitAction(e){
@@ -17,13 +18,14 @@ function submitAction(e){
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
   fetch(url)
-    .then(response =>  response.json()   )
+    .then(response => response.json()    )
     
     .then(data => {
       const { main, name, sys, weather } = data;
       const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
         weather[0]["icon"]
       }.svg`;
+      
 
       const li = document.querySelector("li");
       li.classList.add("city");
@@ -33,7 +35,7 @@ function submitAction(e){
           <span>${name}</span>
           <sup>${sys.country}</sup>
         </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <div class="city-temp">${ Math.round(main.temp)}<sup>°C</sup></div>
         <figure>
           <img class="city-icon" src="${icon}" alt="${
         weather[0]["description"]
@@ -41,12 +43,13 @@ function submitAction(e){
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
       `;
+     
       li.innerHTML = markup;
       list.appendChild(li);
     })
-    .catch(() => {
+    .catch(  () => {
       msg.textContent = "Please search for a valid city";
-    });
+    }  );
 
   msg.textContent = "";
   form.reset();
