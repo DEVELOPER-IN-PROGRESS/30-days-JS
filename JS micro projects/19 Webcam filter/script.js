0e97d7961deebf0c3c11435f5c75701694835be5
iceweasel <johnsjosephk@protonmail.com>
2021-06-15 23:30:24 +0530

    video feed loaded to browser

---
+++ JS micro projects/19 Webcam filter/script.js
@@ -0,0 +1,31 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video:true , audio:false})
+    .then(localMediaStream =>{ 
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        //console.log(localMediaStream.src);
+        video.play();
+    })
+    .catch(err=>{
+        console.error(`Access Denied , Pal` ,err );
+    })
+}
+
+getVideo();
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight ;
+    
+    canvas.width = width ;
+    canvas.height = height ; 
+
+}
+
+paintToCanvas();
