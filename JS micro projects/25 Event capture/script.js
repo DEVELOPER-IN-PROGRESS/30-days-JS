a324787f4441806a7215e419333a912dd576d310
iceweasel <johnsjosephk@protonmail.com>
2021-06-22 23:43:50 +0530

    event propogation and bubbling

---
+++ JS micro projects/25 Event capture/script.js
@@ -0,0 +1,18 @@
+const divs = document.querySelectorAll('div');
+const button =document.querySelector('button');
+function logText(e){
+   // console.log(this.classList.value);
+    console.log(this)
+    // research more on bubbling 
+  //  e.stopPropogation(); // stops bubbling 
+}
+
+divs.forEach( div=> {
+    div.addEventListener('click' ,logText, {capture : false , once:true});
+}) ;
+
+button.addEventListener('click' , ()=>{console.log('BUtton CLicked ')} ,{once: true})
+//the capture happens from a top down approach whereas the events are fired in reverse order
+// capture default is false
+
+//setting once: true will remove event listner after  event is listened
