fc3618bdb742133ecd7a7f8269d02c3a99bf5c0f
iceweasel <johnsjosephk@protonmail.com>
2021-06-23 23:24:11 +0530

    converted ul elements  to flex panel

---
+++ sample websites/animated navigation/script.js
@@ -0,0 +1,20 @@
+const menuBars = document.getElementById('menu-bars') ;
+const overlay = document.getElementById('overlay') ;
+
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+
+function toggleNav(){
+    
+}
+
+
+menuBars.addEventListener ('click' ,toggleNav);
+nav1.addEventListener ('click' ,toggleNav);
+nav2.addEventListener ('click' ,toggleNav);
+nav3.addEventListener ('click' ,toggleNav);
+nav4.addEventListener ('click' ,toggleNav);
+nav5 .addEventListener ('click' ,toggleNav);
