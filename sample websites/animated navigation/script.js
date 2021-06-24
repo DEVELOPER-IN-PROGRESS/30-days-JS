4768af31b1c29356e25ea96c7d1e9687d5d5cd74
iceweasel <johnsjosephk@protonmail.com>
2021-06-24 21:51:53 +0530

    added navigation functionality  reduced code

--- sample websites/animated navigation/script.js
+++ sample websites/animated navigation/script.js
@@ -1,31 +1,41 @@
 const menuBars = document.getElementById('menu-bars') ;
 const overlay = document.getElementById('overlay') ;
-
 const nav1 = document.getElementById('nav-1');
 const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
+const navItems= [nav1, nav2, nav3 ,nav4 ,nav5] ;
+
+function navAnimation(direction1 , direction2) {
+  navItems.forEach( (nav, i) => {
+     //console.log(`slide-${direction1}-${id+1}` , `slide-${direction2}-${id+1}`);
+     nav.classList.replace(`slide-${direction1}-${i+1}` , `slide-${direction2}-${i+1}`);
+  }) ;
+}
+
+
 function toggleNav(){
   // toggle menu bars  
   menuBars.classList.toggle('change')  
   overlay.classList.toggle('overlay-active');
 
+ 
   if (overlay.classList.contains('overlay-active')){
-    overlay.classList.remove('overlay-slide-left'); overlay.classList.add('overlay-slide-right');
+    //Animate overlay 
+   overlay.classList.replace('overlay-slide-left' ,'overlay-slide-right') ;
+// repalce does the combined functionality of remove() and add() 
+// but we cannot replace a class with another if it is not there so we add extra class in HTML 
+      navAnimation('out','in');
+      nav1.classList.replace('slide-out-1', 'slide-in-1');
    }
     else {
-  overlay.classList.remove('overlay-slide-right') ;
-  overlay.classList.add('overlay-slide-left')
+  overlay.classList.replace('overlay-slide-right','overlay-slide-left') ;
+  navAnimation('in' , 'out');
     }
 
-}
-
+  }
 
 menuBars.addEventListener ('click' ,toggleNav);
-nav1.addEventListener ('click' ,toggleNav);
-nav2.addEventListener ('click' ,toggleNav);
-nav3.addEventListener ('click' ,toggleNav);
-nav4.addEventListener ('click' ,toggleNav);
-nav5 .addEventListener ('click' ,toggleNav);
+navItems.forEach( (nav) =>{nav.addEventListener('click' ,toggleNav)}  )
