eae699d2eb4a1880f205e725748b85594604004a
iceweasel <johnsjosephk@protonmail.com>
2021-06-26 23:18:32 +0530

    added localstorage functionality for initial state

--- sample websites/lightdark-mode/script.js
+++ sample websites/lightdark-mode/script.js
@@ -6,14 +6,18 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box')
 
+ function imageMode(color){
+    image1.src= `img/undraw_proud_coder_${color}.svg` ;
+    image2.src= `img/undraw_conceptual_idea_${color}.svg`;
+    image3.src= `img/undraw_feeling_proud_${color}.svg`;
+ }
+
 function darkMode(){
     nav.style.backgroundColor ='rgb(0 0 0 / 50% )';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'DARK Mode';
     toggleIcon.children[1].classList.replace('fa-sun' ,'fa-moon');
-    image1.src= 'img/undraw_proud_coder_dark.svg' ;
-    image2.src= 'img/undraw_conceptual_idea_dark.svg';
-    image3.src= 'img/undraw_feeling_proud_dark.svg';
+    imageMode('dark');
 }
 
 function lightMode(){
@@ -21,20 +25,31 @@ function lightMode(){
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'LIGHT Mode';
     toggleIcon.children[1].classList.replace('fa-moon' ,'fa-sun');
-    image1.src= 'img/undraw_proud_coder_light.svg' ;
-    image2.src= 'img/undraw_conceptual_idea_light.svg';
-    image3.src= 'img/undraw_feeling_proud_light.svg';
+    imageMode('light')
 }
   
 function switchTheme(e){
     if(e.target.checked){
         document.documentElement.setAttribute('data-theme','dark')   ;
+        localStorage.setItem('theme' ,'dark');
         darkMode();
     }
     else{
         document.documentElement.setAttribute('data-theme','light');
+        localStorage.setItem('theme' ,'light');
         lightMode();
     }
 }
 
 toggleSwitch.addEventListener('change' ,switchTheme);
+
+//check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+  if(currentTheme){
+    document.documentElement.setAttribute('data-theme',currentTheme)   ;
+   
+     if(currentTheme ==='dark'){
+         toggleSwitch.checked = true ;
+         darkMode() ;
+     }
+  }
