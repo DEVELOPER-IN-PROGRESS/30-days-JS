cb158087e0eb83a45d9534121ccdb861c42b48c7
iceweasel <johnsjosephk@protonmail.com>
2021-06-30 23:50:33 +0530

    previous and next song click  functionality complete

--- sample websites/Music Player/script.js
+++ sample websites/Music Player/script.js
@@ -1,7 +1,33 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+const songs= [
+    {
+        name :'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artists:'Jaconto Design',
+    },
+    {
+        name :'jacinto-2',
+        displayName: 'Seven Nation Army',
+        artists:'Jaconto Design',
+    },
+    {
+        name :'jacinto-3',
+        displayName: 'Electric Chill Machine',
+        artists:'Jaconto Design',
+    },
+    {
+        name :'jacinto-4',
+        displayName: 'Electric Chill Machine',
+        artists:'Jaconto Design',
+    },
+]
 
 let isPlaying = false;
 
@@ -22,3 +48,43 @@ function pauseSong(){
 
 //Play or Pause event Listener
 playBtn.addEventListener('click' , () =>( isPlaying ? pauseSong() :playSong()));
+
+//Update DOM
+function loadSong(song){
+    title.textContent = song.displayName;
+   artist.textContent = song.artists;
+    music.src= `./music/${song.name}.mp3`;
+    image.src = `./img/${song.name}.jpg`;
+ console.log(song.artists);
+}
+
+//currentsong
+let songIndex = 0 ;
+
+//previos song
+function prevSong(){
+    songIndex--;
+    if(songIndex<0){
+        songIndex= songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+//next song
+function nextSong(){
+    songIndex++;
+    if(songIndex> songs.length -1){
+        songIndex=0;
+    }
+   loadSong(songs[songIndex]);
+    playSong();
+}
+
+//onload Select 1st Song
+loadSong(songs[songIndex]);
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+// document.addEventListener('DOMContentloaded', ()=>loadSong(songs[songIndex]));
