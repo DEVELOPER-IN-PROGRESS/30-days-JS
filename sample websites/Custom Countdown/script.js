fba759c2930322c0c1cb17cb21581d7940e746c8
iceweasel <johnsjosephk@protonmail.com>
2021-07-01 23:36:52 +0530

    fetching form values

--- sample websites/Custom Countdown/script.js
+++ sample websites/Custom Countdown/script.js
@@ -2,7 +2,23 @@ const inputContainer = document.getElementById('input-comtainer');
 const  countdownForm  = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle ='' ;
+let countdownDate = '' ;
+
 //SEt date input minimum 
 const today = new Date().toISOString().split('T')[0];
 console.log(today);
 dateEl.setAttribute('min',today)
+
+//take value from form input
+function updateCountdown(e){
+    countdownTitle = e.srcElement[0].value ;
+    countdownDate = e.srcElement[1].value ;
+    console.log(countdownDate , countdownTitle);
+
+    e.preventDefault();
+}
+
+
+//Event listener
+countdownForm.addEventListener('submit', updateCountdown)
