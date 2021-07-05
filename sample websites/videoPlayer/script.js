4c52a91298abb91cfec7027b4ea69f824a3686f4
iceweasel <johnsjosephk@protonmail.com>
2021-07-05 23:43:34 +0530

    play pause functionality

--- sample websites/videoPlayer/script.js
+++ sample websites/videoPlayer/script.js
@@ -1,7 +1,34 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration  = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon(){
+    playBtn.classList.replace('fa-pause' , 'fa-play');
+         playBtn.setAttribute('title' ,'Play');
+} 
 
+function togglePlay(){
+     if(video.paused){
+        playBtn.classList.replace('fa-play' , 'fa-pause'); 
+        playBtn.setAttribute('title' ,'Pause');
+        video.play();
+
+     }
+     else{
+         video.pause();
+        showPlayIcon();
+     }
+ }
+ //on video end show play button icon 
+ video.addEventListener('ended' , showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
@@ -18,4 +45,6 @@
 
 // Fullscreen ------------------------------- //
 
-
+//Event listeners
+playBtn.addEventListener('click' , togglePlay);
+video.addEventListener('click' , togglePlay);
