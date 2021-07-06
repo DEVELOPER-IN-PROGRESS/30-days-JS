1c22ddc752d6d98dc13eafedd81d9772e92356bd
iceweasel <johnsjosephk@protonmail.com>
2021-07-06 22:54:02 +0530

    video mute feature

--- sample websites/videoPlayer/script.js
+++ sample websites/videoPlayer/script.js
@@ -59,7 +59,7 @@ function setProgress(e){
 
 
 // Volume Controls --------------------------- //
-
+let lastVolume = 1 ; 
 //Volume bar 
 function changeVolume(e){
     let volume = e.offsetX /volumeRange.offsetWidth ;
@@ -81,6 +81,25 @@ function changeVolume(e){
     }else if ( volume === 0 ){
         volumeIcon.classList.add('fas', 'fa-volume-off');
     }
+ lastVolume = volume 
+
+}
+// Mute /Unmute 
+function muteTrigger(){
+    volumeIcon.className = '';
+      if( video.volume ){
+          lastVolume = video.volume ; 
+          video.volume = 0 ;
+          volumeBar.style.width = 0 ; 
+          volumeIcon.classList.add('fas', 'fa-volume-mute');
+          volumeIcon.setAttribute('title' , 'Unmute');
+      }
+      else {
+          video.volume = lastVolume;
+          volumeBar.style.width = `${lastVolume*100}%` ;
+          volumeIcon.classList.add('fas', 'fa-volume-up');
+          volumeIcon.setAttribute('title' , 'Mute');
+      }
 }
 
 // Change Playback Speed -------------------- //
@@ -96,3 +115,4 @@ video.addEventListener('timeupdate' , updateProgress) ;
 video.addEventListener('canplay' , updateProgress) ;
 progressRange.addEventListener('click' ,setProgress );
 volumeRange.addEventListener('click' ,changeVolume) ;
+volumeIcon.addEventListener('click' , muteTrigger) ;
