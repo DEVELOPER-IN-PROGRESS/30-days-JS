6f33bccf6fe7075aa5467270093fcb41a48e3821
iceweasel <johnsjosephk@protonmail.com>
2021-07-10 23:59:26 +0530

    player , computer variables function defined

--- sample websites/rock paper scissors lizard spok game/script.js
+++ sample websites/rock paper scissors lizard spok game/script.js
@@ -1,3 +1,22 @@
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerChoiceEl = document.getElementById('computerChoice');
+const computerScoreEl = document.getElementById('computerScore');
+const resultText = document.getElementById('resultText');
+
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
+
+const allGameIcons = document.getElementById('.far');
 
 
 const choices = {
@@ -8,3 +27,6 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+function select(playerChoice){
+  console.log(playerChoice);
+}
