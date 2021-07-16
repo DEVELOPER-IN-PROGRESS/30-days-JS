33f1feb776d3ce4bf2c25badafa4dffe5fb9ce40
iceweasel <johnsjosephk@protonmail.com>
2021-07-16 23:29:33 +0530

    add favorites to localstorage

--- sample websites/Nasa Apod/script.js
+++ sample websites/Nasa Apod/script.js
@@ -13,54 +13,69 @@ const apiUrl =`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${cou
 let resultsArray =[] ;
 let favourites = {} ;
 
-function updateDOM() {
-resultsArray.forEach( (result)=> { 
-    // Card container 
-    const card = document.createElement('div');
-    card.classList.add('card');
-    //link
-    const link = document.createElement('a');
-    link.href = result.hdurl;
-    link.title = 'View full Image';
-    link.target = '_blank' ;
-    // Image
-    const image = document.createElement('img');
-    image.src = result.url ;
-    image.alt = 'Nasa Picture of the day' ;
-    image.loading = 'lazy' ;
-    image.classList.add('card-img-top');   
-    //Card Body
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-     // Card Title
-    const cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = result.title ;
-    //Save Text
-    const saveText = document.createElement('p');
-    saveText.classList.add('clickable');
-    saveText.textContent = 'Add to Favourites' ;
-    saveText.setAttribute('onclick' , `saveFavorite('${result.url}')` );
-     // Card Text 
-     const cardText = document.createElement('p');
-     cardText.textContent = result.explanation ; 
-     //footer Container ;
-     const footer = document.createElement('small');
-     footer.classList.add('text-muted');
-     //Date
-     const date = document.createElement('strong');
-     date.textContent = result.date ;
-     //Copyright
-     const copyrightResult = result.copyright === undefined ? ' '  : result.copyright ; 
-     const copyright = document.createElement('span');
-     copyright.textContent = `${copyrightResult}` ;
-     // Append
-     footer.append( date , copyright);
-     cardBody.append(cardTitle ,  saveText , cardText ,footer) ;
-     link.appendChild(image);
-     card.append(link , cardBody);
-    imagesContainer.appendChild(card) ;
-}) ;
+function createDOMNodes(page) {
+    const currentArray =  page === 'results' ? resultsArray : Object.values(favourites) ;
+    console.log('Current array' , page , currentArray);  
+    console.log(page);
+
+    currentArray.forEach( (result)=> { 
+        // Card container 
+        const card = document.createElement('div');
+        card.classList.add('card');
+        //link
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View full Image';
+        link.target = '_blank' ;
+        // Image
+        const image = document.createElement('img');
+        image.src = result.url ;
+        image.alt = 'Nasa Picture of the day' ;
+        image.loading = 'lazy' ;
+        image.classList.add('card-img-top');   
+        //Card Body
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+         // Card Title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title ;
+        //Save Text
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        saveText.textContent = 'Add to Favourites' ;
+        saveText.setAttribute('onclick' , `saveFavorite('${result.url}')` );
+         // Card Text 
+         const cardText = document.createElement('p');
+         cardText.textContent = result.explanation ; 
+         //footer Container ;
+         const footer = document.createElement('small');
+         footer.classList.add('text-muted');
+         //Date
+         const date = document.createElement('strong');
+         date.textContent = result.date ;
+         //Copyright
+         const copyrightResult = result.copyright === undefined ? ' '  : result.copyright ; 
+         const copyright = document.createElement('span');
+         copyright.textContent = `${copyrightResult}` ;
+         // Append
+         footer.append( date , copyright);
+         cardBody.append(cardTitle ,  saveText , cardText ,footer) ;
+         link.appendChild(image);
+         card.append(link , cardBody);
+        imagesContainer.appendChild(card) ;
+    }) ;
+}
+
+
+function updateDOM(page) {
+    //get Favourites from localstorage
+    if(localStorage.getItem('nasaFavorites')){
+        favourites  = JSON.parse(localStorage.getItem('nasaFavorites'));
+        console.log('favorites from local storage' ,favourites);
+    }
+   createDOMNodes(page);
+
 }
 
 //Get random 10 images
@@ -68,8 +83,7 @@ async function getNasaPictures(){
     try{
        const  response =  await fetch(apiUrl);
        resultsArray = await response.json();
-       console.log(resultsArray);
-        updateDOM();
+        updateDOM('favorites');
        
     }
     catch(error){
