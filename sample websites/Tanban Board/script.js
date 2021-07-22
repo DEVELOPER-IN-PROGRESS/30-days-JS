c8f93b9e3f992a1f80381a4d8ef3e889b3ef8373
iceweasel <johnsjosephk@protonmail.com>
2021-07-22 23:46:06 +0530

    adding items to the list

--- sample websites/Tanban Board/script.js
+++ sample websites/Tanban Board/script.js
@@ -102,6 +102,28 @@ function updateDOM() {
  updateSavedColumns();
 
 }
+// Add to column List Reset Textbox
+function addToColumn(column){
+ const itemText = addItems[column].textContent ;
+ const selectedArray = listArrays[column];
+ selectedArray.push(itemText);
+ addItems[column].textContent =' ';
+ updateDOM();
+}
+
+// Show add item Input box
+function showInputBox(column){
+    addBtns[column].style.visibility = 'hidden' ;
+    saveItemBtns[column].style.display = 'flex' ; 
+    addItemContainers[column].style.display = 'flex';
+}
+
+function hideInputBox(column){
+  addBtns[column].style.visibility = 'visible' ;
+    saveItemBtns[column].style.display = 'none' ; 
+    addItemContainers[column].style.display = 'none';
+    addToColumn(column);
+}
 
 //Allow arrays to reflect drag  and drop items 
  function rebuildArrays(){ 
